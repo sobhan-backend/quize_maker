1fa82da4f7d978a2db349f3a3049a5523755ad47
sobhan-backend <sobhan.amini.1381@gmail.com>
2025-10-29 13:21:15 +0330

    fix gitignore

---
+++ function/db.php
@@ -0,0 +1,15 @@
+<?php
+
+// $servername = "localhost";
+// $username   = "root";
+// $password   = "";
+// $dbname     = "quize";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $conn;
+} catch(PDOException $e) {
+    die("خطا در اتصال به دیتابیس: " . $e->getMessage());
+}
